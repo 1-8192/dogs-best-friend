b2c230322116681df45ea9a735ebbb730d7e2a65
1-8192 <allegranzia@gmail.com>
2019-05-24 12:17:24 -0400

    added sort based on need

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer =() =>{
   return (
-    <footer className="is-fixed-bottom is-mobile">
+    <footer className="box is-fixed-bottom is-mobile">
       <div className="content has-text-centered">
         <strong>Dog's Best Friend</strong> by Alessandro Allegranzi. © 2019
       </div>
--- src/containers/Doglist.js
+++ src/containers/Doglist.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react'
 import { connect }  from 'react-redux'
 import DogCard from '../components/DogCard'
 
 //Actions
 import { fetchDogs } from '../redux/dogActions'
 
-class Doglist extends React.Component {
+class Doglist extends Component {
 
   state = {
     displayDogs: []
@@ -15,11 +15,20 @@ class Doglist extends React.Component {
     this.props.fetchDogs('http://localhost:3005/api/v1/dogs')
   }
 
+  handleClick = () => {
+    this.props.fetchDogs('http://localhost:3005/api/v1/need')
+  }
+
   render () {
     return (
-      <div className="columns is-multiline is-3-mobile is-3-desktop">
-        {this.props.dogs.map(singleDog => <DogCard key={singleDog.id} dog={singleDog} />)}
-      </div>
+      <Fragment>
+        <div className="box has-text-centered">
+          <button onClick={this.handleClick} className="button is-link is-rounded">Show me dogs who need my help!</button><br/>
+        </div>
+        <div className="columns is-multiline is-3-mobile is-3-desktop">
+          {this.props.dogs.map(singleDog => <DogCard key={singleDog.id} dog={singleDog} />)}
+        </div>
+      </Fragment>
     )
   }
 }
