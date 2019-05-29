e65c82e6fe0643e58b616a29bc30b1c300397afa
1-8192 <allegranzia@gmail.com>
2019-05-29 19:49:35 -0400

    fixed typo on about page

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = () => {
         <br/>
         <p>Adopting or fostering a dog is a substantial commitment, and not one everybody can afford. But NYC shelter dogs still need our help!</p><br/>
         <p>Dog's Best Friend lets you browse real, local shelter dogs and donate anything you can spare in their name, giving them
-        a better chance to find a permament home. Payments are secure, and 100% of your donation goes directly to the animal shelter housing the dog you choose to support,
+        a better chance to find a permanent home. Payments are secure, and 100% of your donation goes directly to the animal shelter housing the dog you choose to support,
         and specifically in the dog's name.</p><br/>
         <p> The dogs featured on this site are real NY shelter dogs scraped from the <a href="https://www.nycacc.org/">Animal Care Centers of NYC</a> website.</p><br/>
       </div>
